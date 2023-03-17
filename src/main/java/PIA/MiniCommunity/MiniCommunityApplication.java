3bae90e051cafdd106179d51df50a90f22f5c676
kcl4419 <kcl4419@gmail.com>
2023-03-17 20:53:08 +0900

    first commit

---
+++ src/main/java/PIA/MiniCommunity/MiniCommunityApplication.java
@@ -0,0 +1,13 @@
+package PIA.MiniCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PIA/MiniCommunity/MiniCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package PIA.MiniCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
